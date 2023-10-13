5b4300e6eac577ea456014bb325fb72c79738333
Coralie <immacora@gmail.com>
2023-10-13 20:24:20 +0200

    build(app): update settings for django version

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -112,3 +112,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
