47609843ba0e3b07207278678b83e52d394fd09f
Coralie <immacora@gmail.com>
2023-10-14 07:07:39 +0200

    refactor(project): add profiles app; move Profile model from oc_lettings_site to profiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local
-    "lettings.apps.LettingsConfig"
+    "lettings.apps.LettingsConfig",
+    "profiles.apps.ProfilesConfig"
 ]
 
 MIDDLEWARE = [
