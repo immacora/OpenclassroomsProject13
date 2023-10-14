5c6755e3c8e62ef1633b1257c0c7b7c4db2fcfe7
Coralie <immacora@gmail.com>
2023-10-14 18:50:21 +0200

    feat(templates): add 404 and 500 custom templates

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("oc_lettings_site.urls")),
     path("lettings/", include("lettings.urls")),
-    path("profiles/", include("profiles.urls"))
+    path("profiles/", include("profiles.urls")),
 ]
+
+handler404 = "oc_lettings_site.views.custom_404_view"  # noqa F811
+handler500 = "oc_lettings_site.views.custom_500_view"  # noqa F811
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'oc_lettings_site/index.html')
+
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def custom_500_view(request):
+    return render(request, '500.html', status=500)
