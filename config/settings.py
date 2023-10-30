37a40e28cf8851018249707c01d9ef37b6015b1d
Coralie <immacora@gmail.com>
2023-10-30 13:26:57 +0100

    build(project): add gunicorn; update project settings for env var and logging; update docker image for hub

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -14,12 +15,33 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+def get_env_bool(name, default="False"):
+    """Checks if environment variable is equal to "true" and returns bool value."""
+    return os.environ.get(name, default).lower() == "true"
 
-ALLOWED_HOSTS = []
+
+DEBUG = get_env_bool("DEBUG")
+
+CSRF_COOKIE_SECURE = not DEBUG
+
+SESSION_COOKIE_SECURE = not DEBUG
+
+if DEBUG:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+else:
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+    if not ALLOWED_HOSTS:
+        raise ValueError(
+            "ALLOWED_HOSTS is empty. Check your environment configuration."
+        )
 
 
 # Application definition
@@ -126,26 +148,21 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+# Sentry SDK config
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
     integrations=[DjangoIntegration()],
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
+    traces_sample_rate=0.5,
+    profiles_sample_rate=0.5,
 )
 
 
-LOGGING_DIR = "log"
-if not os.path.exists(LOGGING_DIR):
-    os.makedirs(LOGGING_DIR)
-
+# Logging config
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "verbose": {
-            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-            "style": "{",
-        },
         "simple": {
             "format": "{levelname} {message}",
             "style": "{",
@@ -163,22 +180,11 @@ LOGGING = {
             "class": "logging.StreamHandler",
             "formatter": "simple",
         },
-        "file": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "filename": "log/debug.log",
-            "formatter": "verbose",
-        },
-        "sentry": {
-            "level": "INFO",
-            "class": "sentry_sdk.integrations.logging.SentryHandler",
-            "formatter": "verbose",
-        },
     },
     "loggers": {
         "django": {
-            "handlers": ["file", "console", "sentry"],
-            "level": "INFO",
+            "handlers": ["console"],
+            "level": "ERROR",
             "propagate": True,
         },
     },
